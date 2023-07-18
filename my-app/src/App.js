8bf33a1673d7233634bb9d21e23a89c1b868b9e6
SusancheZzz <sasha_m03@list.ru>
2023-07-18 22:01:05 +0300

    Final version project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
+
 import './App.css';
-import background from "./background.jpg";
+
+const slides = [
+  { title: 'Слайд 1' },
+  { title: 'Слайд 2' },
+  { title: 'Слайд 3' },
+  { title: 'Слайд 4' },
+  { title: 'Слайд 5' },
+  { title: 'Слайд 6' },
+];
 
 const App = () => {
   const [selectedSlide, setSelectedSlide] = useState(null);
@@ -9,16 +18,14 @@ const App = () => {
   const [bottomText, setBottomText] = useState('');
   const [showModifiedImage, setShowModifiedImage] = useState(false);
   const [isBlackAndWhite, setIsBlackAndWhite] = useState(false);
-  const [IsInversImage, setIsInversImage] = useState(false);
+  const [isInverted, setIsInverted] = useState(false);
 
   const handleSlideClick = (index) => {
     if (selectedSlide === index) {
-// Если выбран уже выбранный слайд, обнуляем выбранный слайд и изображение
       setSelectedSlide(null);
       setSelectedImage(null);
       setShowModifiedImage(false);
     } else {
-// Если выбран новый слайд, обновляем выбранный слайд и изображение
       setSelectedSlide(index);
       setSelectedImage(`img/image${index + 1}.jpg`);
       setShowModifiedImage(false);
@@ -34,59 +41,45 @@ const App = () => {
   };
 
   const handleApplyButtonClick = () => {
-    if (selectedImage) {
-// Здесь вы можете выполнить необходимую обработку, чтобы добавить текст к изображению.
-// Например, вы можете использовать библиотеку для обработки изображений или создать изображение с помощью canvas.
+    const modifiedImage = document.createElement('img');
+    modifiedImage.src = selectedImage;
 
-// В данном случае просто обновим выбранное изображение с текстом.
-      const modifiedImage = document.createElement('img');
-      modifiedImage.src = selectedImage;
+    const canvas = document.createElement('canvas');
+    canvas.width = modifiedImage.width;
+    canvas.height = modifiedImage.height;
 
-      const canvas = document.createElement('canvas');
-      canvas.width = modifiedImage.width;
-      canvas.height = modifiedImage.height;
+    const context = canvas.getContext('2d');
+    context.drawImage(modifiedImage, 0, 0);
+    context.font = '20px Arial';
+    context.fillStyle = 'white';
+    context.textAlign = 'center';
+    context.fillText(topText, canvas.width / 2, 40);
+    context.fillText(bottomText, canvas.width / 2, canvas.height - 40);
 
-      const context = canvas.getContext('2d');
-      context.drawImage(modifiedImage, 0, 0);
-      context.font = '20px Arial';
-      context.fillStyle = 'white';
-      context.textAlign = 'center';
-      context.fillText(topText, canvas.width / 2, 40);
-      context.fillText(bottomText, canvas.width / 2, canvas.height - 40);
+    const modifiedImageUrl = canvas.toDataURL('image/jpeg');
 
-// сохранить модифицированное изображение и т.д.
-// В данном случае просто обновим выбранное изображение для отображения в приложении.
-      const modifiedImageUrl = canvas.toDataURL('image/jpeg');
-      setSelectedImage(modifiedImageUrl);
-      setShowModifiedImage(true);
-    } else {
-      if (topText || bottomText) {
-// Выводим подсказку, если отсутствует выбранное изображение, но есть текст в полях
-        alert('you need to choose an image');
-      }
-    }
+    setSelectedImage(modifiedImageUrl);
+    setShowModifiedImage(true);
   };
+
   const handleToggleBlackAndWhite = () => {
     setIsBlackAndWhite((prevValue) => !prevValue);
+    setIsInverted(false); // Сбросить другие фильтры
   };
 
-  const handleInversImage = () => {
-    setIsInversImage((preVValue) => !preVValue);
+  const handleToggleInverted = () => {
+    setIsInverted((prevValue) => !prevValue);
+    setIsBlackAndWhite(false); // Сбросить другие фильтры
   };
 
-  const slides = [
-    { title: 'Слайд 1' },
-    { title: 'Слайд 2' },
-    { title: 'Слайд 3' },
-    { title: 'Слайд 4' },
-    { title: 'Слайд 5' },
-    { title: 'Слайд 6' },
-  ];
+  const handleResetFilters = () => {
+    setIsBlackAndWhite(false);
+    setIsInverted(false);
+  };
 
   return (
-      <div className="container" style={{ backgroundImage: `url(${background})` }}>
-
-          <div className="slider" >
+      <div className="container">
+        <div className="slider">
           {slides.map((slide, index) => (
               <div
                   key={index}
@@ -101,9 +94,8 @@ const App = () => {
               </div>
           ))}
         </div>
-
-          <div className="otstup"></div>
-          <div className="button-container">
+        <div className="otstup"></div>
+        <div className="button-container">
           <div className="top-left">
             <div className="input-row">
               <input
@@ -118,30 +110,33 @@ const App = () => {
                   value={bottomText}
                   onChange={handleBottomTextChange}
               />
-
               <button className="apply-button" onClick={handleApplyButtonClick}>
-                GO to Meme
+                GO
               </button>
             </div>
-
-            <div className="input-row">
-              <button className="apply-button" onClick={handleToggleBlackAndWhite}>
-                Toggle B/W
+            <div className="filter-buttons">
+              <button
+                  className={`filter-button ${isBlackAndWhite ? 'active' : ''}`}
+                  onClick={handleToggleBlackAndWhite}
+              >
+                B/W
               </button>
-              <h5>
-                <font color="blue">..</font>
-              </h5>
-              <button className="apply-button" onClick={handleInversImage}>
-                Сlick to Invert
+              <button
+                  className={`filter-button ${isInverted ? 'active' : ''}`}
+                  onClick={handleToggleInverted}
+              >
+                Inverted
+              </button>
+              <button className="reset-button" onClick={handleResetFilters}>
+                Reset Filters
               </button>
             </div>
-
           </div>
           <div className="top-right">
             {showModifiedImage && selectedImage && (
                 <img
                     src={selectedImage}
-                    className={'selected-image ' + (isBlackAndWhite ? 'black-and-white' : (IsInversImage ? 'invert' : ''))}
+                    className={`selected-image ${isBlackAndWhite ? 'black-and-white' : ''} ${isInverted ? 'inverted' : ''}`}
                     alt="Selected"
                 />
             )}
@@ -150,4 +145,5 @@ const App = () => {
       </div>
   );
 };
+
 export default App;
